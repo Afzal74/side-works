bc123290dd9d67475bd6490fdec67c0e52c68d7c
Afzal Basheer <appuafzal777@gmail.com>
2025-04-06 15:50:03 +0530

    Digital Clock

---
+++ Digital_clock/script.js
@@ -0,0 +1,19 @@
+function updateClock(){
+    const now = new Date();
+    let hours = now.getHours();
+    const Meridiam = hours>=12? "am ": "pm" ;
+    hours = hours % 12  || 12;
+    hours = hours.toString().padStart(2,0);
+    const minutes = now.getMinutes().toString().padStart(2,0);
+    const seconds = now.getSeconds().toString().padStart(2,0);
+    const timeString = `${hours}:${minutes}:${seconds} ${Meridiam}`
+    document.getElementById("clock").textContent = timeString;
+    console.log(now);
+}
+
+//to update every seconds
+
+
+updateClock();
+
+setInterval(updateClock,1000)
